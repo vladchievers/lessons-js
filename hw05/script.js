7d54b32a237eedabd3220fe33063f60a6548f4f6
vladchievers <31140441+vladchievers@users.noreply.github.com>
2018-03-26 16:20:23 +0300

    hw05 while

--- hw05/script.js
+++ hw05/script.js
@@ -1,14 +1,14 @@
 var num = +prompt("Введите число");
 var a = "";
 
-if(!num){
-	alert("Можно вводить только числа!!")
-}else{
-	for (var i = 1; i <= num; i++) {
-		for (var j = 0; j < i; j++) {
-			a = a + "*";
-		};
-		a = a + "\n";
+while(!num){
+	var num = +prompt("Введите число");
+}
+
+for (var i = 1; i <= num; i++) {
+	for (var j = 0; j < i; j++) {
+		a = a + "*";
 	};
-	console.log(a);
+	a = a + "\n";
 };
+console.log(a);
