ead043e151bc46c214e39cb41ae1de89a6b040e7
Vlad <31140441+vladchievers@users.noreply.github.com>
2018-05-22 19:00:24 +0300

    hw16_ start

---
+++ hw16/assets/script.js
@@ -0,0 +1,20 @@
+
+
+$('button').on('click', function(){
+	var select = $('select').val();
+	var files = 'files/' + select;
+	$.ajax(files, {
+		beforeSend: function() {
+			$('.loader').show();
+		},
+		success: function(elem){
+			$('body').prepend(elem)
+		},
+		error: function(error) {
+			$('body').prepend(error.responseText);
+		},
+		complete: function() {
+			$('.loader').hide();
+		}
+	});
+});
