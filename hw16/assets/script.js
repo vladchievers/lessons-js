6f45f7d0cc1fa527cf81e93e122981f930c12572
vladchievers <vladchievers@gmail.com>
2018-05-23 22:27:41 +0300

    hw16

--- hw16/assets/script.js
+++ hw16/assets/script.js
@@ -1,54 +1,63 @@
+$(function(){
 
+	$('button').on('click' , function(e){
 
-// $('button').on('click', function(){
-// 	var select = $('select').val();
-// 	var files = 'files/' + select;
-// 	$.ajax(files, {
-// 		beforeSend: function() {
-// 			$('.loader').show();
-// 		},
-// 		success: function(elem){
-// 			$('body').prepend(elem)
-// 		},
-// 		error: function(error) {
-// 			$('body').prepend(error.responseText);
-// 		},
-// 		complete: function() {
-// 			$('.loader').hide();
-// 		}
-// 	});
-// });
+		$(this).remove();
+		$('body').addClass('bg');
 
+		var infoLoad = {
+			data: function() {
+				$loader = $('.loader');
 
-var infoLoad = {
-	data: function() {
-		$wrapper = $('.info-block');
-		$loader = $('.loader');
+				$.ajax('https://swapi.co/api/people', {
+					dataType: 'json',
+					context: this,
 
-		$.ajax('https://swapi.co/api/people', {
-			dataType: 'json',
-			context: infoLoad,
-			crossDomain: true,
-			success: function(res){
-				console.log(res)
-				$wrapper.append('<p>' + res.results[0].name + '</p>')
+					success: function(result){
+						var $result = result.results;
+						var $wrapper = $('<div> </div>');
+						$wrapper.addClass('wrapper');
+
+						$result.map(function(el){
+							var $div = $('<div></div>');
+							$div.addClass('item');
+
+							var $name = $('<h3>' + el.name + '</h3>');
+							var $height = $('<p>' + 'height:  ' + el.height + '</p>');
+							var $mass = $('<p>' + 'weight:  ' + el.mass + '</p>');
+							var $sex = $('<p>' + 'sex:  ' + el.gender + '</p>');
+							
+ 							infoLoad.setData($name, $div);
+							infoLoad.setData($mass, $div);
+							infoLoad.setData($height, $div);
+							infoLoad.setData($sex, $div);
+							infoLoad.setData( $div ,  $wrapper);
+
+							return $wrapper;
+						});
+
+						infoLoad.setData($wrapper, $('body'));
+					},
+
+					error: function(error) {
+						$('body').prepend(error.responseText);
+					},
+
+					beforeSend: function() {
+						$loader.show();
+					},
+
+					complete: function() {
+						$loader.hide();
+					}
+				});
 			},
-			error: function(error) {
-				$('body').prepend(error.responseText);
-			},
-			beforeSend: function() {
-				$loader.show();
-			},
-			complete: function() {
-				$loader.hide();
-			}
-		});
-	},
 
-	
-	setData: function(what, where){
-		where.append(what);
-	}
-}
+			setData: function(what, where){
+				where.append(what);
+			},
+		};
 
-infoLoad.data();
+		infoLoad.data();
+	});
+});
