710057144febd9ffaf27ffd8e1a9392ab746d597
Vlad <31140441+vladchievers@users.noreply.github.com>
2018-05-23 18:36:38 +0300

    hw16_edit

--- hw16/assets/script.js
+++ hw16/assets/script.js
@@ -1,20 +1,54 @@
 
 
-$('button').on('click', function(){
-	var select = $('select').val();
-	var files = 'files/' + select;
-	$.ajax(files, {
-		beforeSend: function() {
-			$('.loader').show();
-		},
-		success: function(elem){
-			$('body').prepend(elem)
-		},
-		error: function(error) {
-			$('body').prepend(error.responseText);
-		},
-		complete: function() {
-			$('.loader').hide();
-		}
-	});
-});
+// $('button').on('click', function(){
+// 	var select = $('select').val();
+// 	var files = 'files/' + select;
+// 	$.ajax(files, {
+// 		beforeSend: function() {
+// 			$('.loader').show();
+// 		},
+// 		success: function(elem){
+// 			$('body').prepend(elem)
+// 		},
+// 		error: function(error) {
+// 			$('body').prepend(error.responseText);
+// 		},
+// 		complete: function() {
+// 			$('.loader').hide();
+// 		}
+// 	});
+// });
+
+
+var infoLoad = {
+	data: function() {
+		$wrapper = $('.info-block');
+		$loader = $('.loader');
+
+		$.ajax('https://swapi.co/api/people', {
+			dataType: 'json',
+			context: infoLoad,
+			crossDomain: true,
+			success: function(res){
+				console.log(res)
+				$wrapper.append('<p>' + res.results[0].name + '</p>')
+			},
+			error: function(error) {
+				$('body').prepend(error.responseText);
+			},
+			beforeSend: function() {
+				$loader.show();
+			},
+			complete: function() {
+				$loader.hide();
+			}
+		});
+	},
+
+	
+	setData: function(what, where){
+		where.append(what);
+	}
+}
+
+infoLoad.data();
