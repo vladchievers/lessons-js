a99032b4f3297504896d6f30be6ff5ecfa583926
vladchievers <vladchievers@gmail.com>
2018-05-28 23:59:09 +0300

    hw16_v2

--- hw16/assets/script.js
+++ hw16/assets/script.js
@@ -1,63 +1,77 @@
 $(function(){
 
-	$('button').on('click' , function(e){
+	var infoLoad = {
+		data: function() {
+			$loader = $('.loader');
+
+			$.ajax('https://swapi.co/api/people', {
+				dataType: 'json',
+				context: this,
+
+				success: function(result){
+					var $result = result.results;
+					var $wrapper = $('<div> </div>');
+					$wrapper.addClass('wrapper');
+
+					$result.map(function(el){
+						var $div = $('<div></div>');
+						$div.addClass('item');
+
+						var $name = $('<h3>' + el.name + '</h3>');
+						var $height = $('<p>' + 'height:  ' + el.height + '</p>');
+						var $mass = $('<p>' + 'weight:  ' + el.mass + '</p>');
+						var $sex = $('<p>' + 'sex:  ' + el.gender + '</p>');
+
+						infoLoad.setData($name, $div);
+						infoLoad.setData($mass, $div);
+						infoLoad.setData($height, $div);
+						infoLoad.setData($sex, $div);
+						infoLoad.setData( $div ,  $wrapper);
+
+						return $wrapper;
+					});
+
+					infoLoad.setData($wrapper, $('body'));
+				},
+
+				error: function(error) {
+					$('body').prepend(error.responseText);
+				},
+
+				beforeSend: function() {
+					$loader.show();
+				},
+
+				complete: function() {
+					$loader.hide();
+				}
+			});
+		},
+
+		setData: function(what, where){
+			where.append(what);
+		},
+	};
+
+
+	$('.main-btn').on('click' , function(e){
 
 		$(this).remove();
 		$('body').addClass('bg');
+		$('.btn-add').show();
+		
+		infoLoad.data();
 
-		var infoLoad = {
-			data: function() {
-				$loader = $('.loader');
 
-				$.ajax('https://swapi.co/api/people', {
-					dataType: 'json',
-					context: this,
+	});
 
-					success: function(result){
-						var $result = result.results;
-						var $wrapper = $('<div> </div>');
-						$wrapper.addClass('wrapper');
 
-						$result.map(function(el){
-							var $div = $('<div></div>');
-							$div.addClass('item');
-
-							var $name = $('<h3>' + el.name + '</h3>');
-							var $height = $('<p>' + 'height:  ' + el.height + '</p>');
-							var $mass = $('<p>' + 'weight:  ' + el.mass + '</p>');
-							var $sex = $('<p>' + 'sex:  ' + el.gender + '</p>');
-							
- 							infoLoad.setData($name, $div);
-							infoLoad.setData($mass, $div);
-							infoLoad.setData($height, $div);
-							infoLoad.setData($sex, $div);
-							infoLoad.setData( $div ,  $wrapper);
-
-							return $wrapper;
-						});
-
-						infoLoad.setData($wrapper, $('body'));
-					},
-
-					error: function(error) {
-						$('body').prepend(error.responseText);
-					},
-
-					beforeSend: function() {
-						$loader.show();
-					},
-
-					complete: function() {
-						$loader.hide();
-					}
-				});
-			},
-
-			setData: function(what, where){
-				where.append(what);
-			},
-		};
-
+	$('.btn-add').on('click' , function(){
 		infoLoad.data();
 	});
+
+
 });
+
+
+
