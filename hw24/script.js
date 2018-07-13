d255a8b0524ee1da083ec89165848deb40653bd6
Vlad <31140441+vladchievers@users.noreply.github.com>
2018-07-13 12:42:00 +0300

    hw24

--- hw24/script.js
+++ hw24/script.js
@@ -3,9 +3,7 @@ class CreateTask {
 	constructor (){
 		let data = localStorage.getItem("todo");
 	
-		this.data = JSON.parse(data) ||{
-			todo : [] 
-		};
+		this.data = JSON.parse(data) ||{ todo : [] };
 	}
 
 	init(){
@@ -14,12 +12,26 @@ class CreateTask {
 		});
 	}
 
-	removeTodo(id) {
-		console.log(id);
+	removeTodo(id) {		
 		const ind = this.data.todo.findIndex(item => item.id == id);
-		console.log(ind);
+		
 		this.data.todo.splice(ind, 1);
 
+		this.save();
+	}
+
+	checked(id) {
+		const ind = this.data.todo.findIndex(item => item.id == id);
+		
+		this.data.todo[ind].check = true;
+		
+		this.save();
+	}
+	unchecked(id) {
+		const ind = this.data.todo.findIndex(item => item.id == id);
+		
+		this.data.todo[ind].check = false;
+		
 		this.save();
 	}
 
@@ -28,25 +40,19 @@ class CreateTask {
 	}
 
 	newTodo() {
-
 		const obj = {
 			id: this.data.todo.length,
 			title: this.newTitle(),
 			check: false
 		};
 
-		console.log(obj)
-
 		this.save(obj);
 		this.add(obj);
 	}
 
 	save(obj) {
-		if (obj) {
-			this.data.todo.push(obj);
-		}
-		
-
+		obj && this.data.todo.push(obj);
+			
 		localStorage.setItem("todo", JSON.stringify(this.data));
 	}
 
@@ -60,38 +66,48 @@ class CreateTask {
 
 		const title = document.createElement('span');
 		title.innerHTML = obj.title;
+		document.querySelector('.input-text').value = "";
 		
 
 		const checkbox = document.createElement('input');
 		checkbox.setAttribute("type", "checkbox");
 
-		obj.check && checkbox.setAttribute('checked');
+		obj.check && checkbox.setAttribute('checked' , 'checked');
 		
 
 		const remove = document.createElement('button');
 		remove.classList.add('remove');
 
-
+		const checkmark = document.createElement('span');
+		checkmark.classList.add('checkmark');
 
 		li.appendChild(checkbox);
 		li.appendChild(title);
 		li.appendChild(remove);
+		li.appendChild(checkmark);
 		ul.appendChild(li);
 
-
+		const self = this;
 		
 
 		checkbox.addEventListener('click' , function() {
-			console.log('1')
+			const id = this.parentElement.dataset.id;
+			if(this.getAttribute('checked')){
+				this.removeAttribute('checked');
+				self.unchecked(id);
+			}else{
+				this.setAttribute('checked' , 'checked');
+				self.checked(id);
+			};
+			
 		});
 
-		const self = this;
+		
 
 		remove.addEventListener('click', function(){
 
 			const id = this.parentElement.dataset.id; 
 
-		
 			self.removeTodo(id);
 
 			this.parentElement.remove();
@@ -105,8 +121,8 @@ const task = new CreateTask();
 task.init();
 
 document.querySelector('.save').addEventListener('click' , () =>{
+	document.querySelector('.input-text').value ? task.newTodo() : alert('Enter the task')
 	
-	task.newTodo();
 	
 })
 
