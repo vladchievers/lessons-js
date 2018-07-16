c9c50f5972f66da7e5cb8c5d317f93f62cb77122
Vlad <31140441+vladchievers@users.noreply.github.com>
2018-07-16 19:13:17 +0300

    hw25

--- hw25/script.js
+++ hw25/script.js
@@ -1,20 +1,65 @@
-function getTime () {
-	const date = new Date();
 
-	let hours = date.getHours();
-	let minutes = date.getMinutes();
-	let seconds = date.getSeconds();
-	
-	if(hours < 10) {hours = '0' + hours};
-	if(minutes < 10) {minutes = '0' + minutes};
-	if(seconds < 10) {seconds = '0' + seconds};
+class Alarm {
+	constructor () {
+		this.alarm = "";
+	};
+	init() {
+		setInterval(() => {
+			const date = new Date();
+			let hours = date.getHours();
+			let minutes = date.getMinutes();
+			let seconds = date.getSeconds();
 
-	let time =  `${hours} : ${minutes} : ${seconds}`;
+			if(hours < 10) {hours = '0' + hours};
+			if(minutes < 10) {minutes = '0' + minutes};
+			if(seconds < 10) {seconds = '0' + seconds};
 
-	document.querySelector('.watch').textContent = time;
-	setInterval( function() {getTime()}, 1000);
+			let time =  `${hours} : ${minutes} : ${seconds}`;
+			let forAlarm = `${hours} : ${minutes}`;
 
-	let alarm_
-}
+			document.querySelector('.watch').textContent = time;
 
-window.addEventListener('onload', getTime());
+			this.setAlarm(forAlarm);
+			
+		}, 1000);
+	};
+
+	setAlarm(time) {
+		document.querySelector('.setAlarm').addEventListener('click', () =>{
+			
+			let hours = document.querySelector('.alarm-hours').value;
+			let minutes = document.querySelector('.alarm-minutes').value;
+
+			if(hours < 10 ) {hours = '0' + hours};
+			if(minutes < 10 ) {minutes = '0' + minutes};
+
+			let alarm = `${hours} : ${minutes}`;
+
+			this.alarm = alarm;
+			document.querySelector('.setAlarm').style.backgroundColor = 'grey';
+		});
+
+		
+		if(this.alarm === time){
+			document.querySelector('audio').play();
+		}
+
+		document.querySelector('.stop').addEventListener('click', () =>{
+			document.querySelector('audio').pause();
+
+			let hours = document.querySelector('.alarm-hours');
+			let minutes = document.querySelector('.alarm-minutes');
+
+			hours.value = 0;
+			minutes.value= 0;
+
+			document.querySelector('.setAlarm').style.backgroundColor = 'lightgreen'
+
+		});
+
+	}
+};
+
+new Alarm().init();
+
+
