0950280b34b6d8f5904ac6d9318cc772f8f66e42
vladchievers <vladchievers@gmail.com>
2018-07-23 00:54:51 +0300

    hw26-start

---
+++ hw26/assets/script.js
@@ -0,0 +1,105 @@
+function createLi (data) {
+	let arr = data.results;
+	console.log(arr)
+	arr.forEach((el) => {
+		
+		let li = ;
+
+		li.textContent = el.name;
+		li.textContent = el.height;
+	});
+	
+	return arr;
+
+}
+function createUl (result) {
+	console.log(result)
+	let ul = document.createElement('ul');
+
+	ul.appendChild(result);
+	document.body.appendChild(ul)
+}
+
+fetch('https://swapi.co/api/people')
+	.then(data => data.json())
+	.then(createLi)
+	.then(createUl)
+	.catch(console.error);
+
+// $(function(){
+
+// 	var infoLoad = {
+// 		data: function() {
+// 			$loader = $('.loader');
+
+// 			$.ajax('https://swapi.co/api/people', {
+// 				dataType: 'json',
+// 				context: this,
+
+// 				success: function(result){
+// 					var $result = result.results;
+// 					var $wrapper = $('<div> </div>');
+// 					$wrapper.addClass('wrapper');
+
+// 					$result.map(function(el){
+// 						var $div = $('<div></div>');
+// 						$div.addClass('item');
+
+// 						var $name = $('<h3>' + el.name + '</h3>');
+// 						var $height = $('<p>' + 'height:  ' + el.height + '</p>');
+// 						var $mass = $('<p>' + 'weight:  ' + el.mass + '</p>');
+// 						var $sex = $('<p>' + 'sex:  ' + el.gender + '</p>');
+
+// 						infoLoad.setData($name, $div);
+// 						infoLoad.setData($mass, $div);
+// 						infoLoad.setData($height, $div);
+// 						infoLoad.setData($sex, $div);
+// 						infoLoad.setData( $div ,  $wrapper);
+
+// 						return $wrapper;
+// 					});
+
+// 					infoLoad.setData($wrapper, $('body'));
+// 				},
+
+// 				error: function(error) {
+// 					$('body').prepend(error.responseText);
+// 				},
+
+// 				beforeSend: function() {
+// 					$loader.show();
+// 				},
+
+// 				complete: function() {
+// 					$loader.hide();
+// 				}
+// 			});
+// 		},
+
+// 		setData: function(what, where){
+// 			where.append(what);
+// 		},
+// 	};
+
+
+// 	$('.main-btn').on('click' , function(e){
+
+// 		$(this).remove();
+// 		$('body').addClass('bg');
+// 		$('.btn-add').show();
+		
+// 		infoLoad.data();
+
+
+// 	});
+
+
+// 	$('.btn-add').on('click' , function(){
+// 		infoLoad.data();
+// 	});
+
+
+// });
+
+
+
