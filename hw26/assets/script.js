353a7963cb1ae4ad81a46b4027a6ef8079a3412f
Vlad <31140441+vladchievers@users.noreply.github.com>
2018-07-25 12:42:13 +0300

    hw26

--- hw26/assets/script.js
+++ hw26/assets/script.js
@@ -1,105 +1,43 @@
 function createLi (data) {
 	let arr = data.results;
-	console.log(arr)
-	arr.forEach((el) => {
-		
-		let li = ;
 
-		li.textContent = el.name;
-		li.textContent = el.height;
-	});
+	let li = arr.reduce((prev , el) => {
+		return prev += `<li>
+							<h3>${el.name}</h3>
+							<p>mass: ${el.mass}</p>
+							<p>height: ${el.height}</p>
+							<p>sex: ${el.gender}</p>
+						</li>`
+	},'');
 	
-	return arr;
+	return li;
+};
 
-}
-function createUl (result) {
-	console.log(result)
-	let ul = document.createElement('ul');
+function createUl (li) {
 
-	ul.appendChild(result);
-	document.body.appendChild(ul)
-}
+	let ul = `<ul>${li}</ul>`
+	
+	return ul;
+};
 
-fetch('https://swapi.co/api/people')
-	.then(data => data.json())
-	.then(createLi)
-	.then(createUl)
-	.catch(console.error);
-
-// $(function(){
-
-// 	var infoLoad = {
-// 		data: function() {
-// 			$loader = $('.loader');
-
-// 			$.ajax('https://swapi.co/api/people', {
-// 				dataType: 'json',
-// 				context: this,
-
-// 				success: function(result){
-// 					var $result = result.results;
-// 					var $wrapper = $('<div> </div>');
-// 					$wrapper.addClass('wrapper');
-
-// 					$result.map(function(el){
-// 						var $div = $('<div></div>');
-// 						$div.addClass('item');
-
-// 						var $name = $('<h3>' + el.name + '</h3>');
-// 						var $height = $('<p>' + 'height:  ' + el.height + '</p>');
-// 						var $mass = $('<p>' + 'weight:  ' + el.mass + '</p>');
-// 						var $sex = $('<p>' + 'sex:  ' + el.gender + '</p>');
-
-// 						infoLoad.setData($name, $div);
-// 						infoLoad.setData($mass, $div);
-// 						infoLoad.setData($height, $div);
-// 						infoLoad.setData($sex, $div);
-// 						infoLoad.setData( $div ,  $wrapper);
-
-// 						return $wrapper;
-// 					});
-
-// 					infoLoad.setData($wrapper, $('body'));
-// 				},
-
-// 				error: function(error) {
-// 					$('body').prepend(error.responseText);
-// 				},
-
-// 				beforeSend: function() {
-// 					$loader.show();
-// 				},
-
-// 				complete: function() {
-// 					$loader.hide();
-// 				}
-// 			});
-// 		},
-
-// 		setData: function(what, where){
-// 			where.append(what);
-// 		},
-// 	};
+function addHTML (ul) {
+	document.querySelector('.wrapper').innerHTML = ul;
+	document.querySelector('.loader').style.display = 'none';
+	document.body.classList.add('bg');
+	document.querySelector('.btn-add').classList.add('after');
+};
 
 
-// 	$('.main-btn').on('click' , function(e){
 
-// 		$(this).remove();
-// 		$('body').addClass('bg');
-// 		$('.btn-add').show();
-		
-// 		infoLoad.data();
+document.querySelector('.btn-add').addEventListener('click' , () => {
+	document.querySelector('.loader').style.display = 'block';
 
-
-// 	});
-
-
-// 	$('.btn-add').on('click' , function(){
-// 		infoLoad.data();
-// 	});
-
-
-// });
-
+	fetch('https://swapi.co/api/people')
+		.then(data => data.json())
+		.then(createLi)
+		.then(createUl)
+		.then(addHTML)
+		.catch(console.error);
+});
 
 
